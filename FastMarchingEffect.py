1f72cdb9d1a60d54ae0e3e58f907d0c21a00d10b
Andrey Fedorov <fedorov@bwh.harvard.edu>
2013-02-10 18:46:52 -0500

    WIP FastMarchingEffect initial commit
    Not functional, and weird-looking GUI :)

---
+++ FastMarchingEffect.py
@@ -0,0 +1,301 @@
+import os
+from __main__ import vtk, qt, ctk, slicer
+import EditorLib
+from EditorLib.EditOptions import HelpButton
+from EditorLib.EditOptions import EditOptions
+from EditorLib import EditUtil
+from EditorLib import LabelEffect
+
+#
+# The Editor Extension itself.
+#
+# This needs to define the hooks to be come an editor effect.
+#
+
+#
+# FastMarchingEffectOptions - see LabelEffect, EditOptions and Effect for superclasses
+#
+
+class FastMarchingEffectOptions(EditorLib.LabelEffectOptions):
+  """ FastMarchingEffect-specfic gui
+  """
+
+  def __init__(self, parent=0):
+    super(FastMarchingEffectOptions,self).__init__(parent)
+
+    # self.attributes should be tuple of options:
+    # 'MouseTool' - grabs the cursor
+    # 'Nonmodal' - can be applied while another is active
+    # 'Disabled' - not available
+    self.attributes = ('MouseTool')
+    self.displayName = 'FastMarchingEffect Effect'
+
+  def __del__(self):
+    super(FastMarchingEffectOptions,self).__del__()
+
+  def create(self):
+    super(FastMarchingEffectOptions,self).create()
+
+    self.marcherLabel = qt.QLabel('March:',self.frame)
+    self.marcherLabel.setToolTip('March over the front propagation timeline')
+    self.frame.layout().addWidget(self.marcherLabel)
+    self.widgets.append(self.marcherLabel)
+
+    self.marcher = ctk.ctkSliderWidget(self.frame)
+    self.marcher.minimum = 0
+    self.marcher.maximum = 1
+    self.marcher.singleStep = 0.01
+    self.widgets.append(self.marcher)
+    self.marcher.connect('valueChanged(double)',self.onMarcherChanged)
+
+    self.apply = qt.QPushButton("Apply", self.frame)
+    self.apply.setToolTip("Apply the extension operation")
+    self.frame.layout().addWidget(self.apply)
+    self.widgets.append(self.apply)
+
+    HelpButton(self.frame, "This is a sample with no real functionality.")
+
+    self.apply.connect('clicked()', self.onApply)
+
+    # Add vertical spacer
+    self.frame.layout().addStretch(1)
+
+  def destroy(self):
+    super(FastMarchingEffectOptions,self).destroy()
+
+  # note: this method needs to be implemented exactly as-is
+  # in each leaf subclass so that "self" in the observer
+  # is of the correct type
+  def updateParameterNode(self, caller, event):
+    node = EditUtil.EditUtil().getParameterNode()
+    if node != self.parameterNode:
+      if self.parameterNode:
+        node.RemoveObserver(self.parameterNodeTag)
+      self.parameterNode = node
+      self.parameterNodeTag = node.AddObserver(vtk.vtkCommand.ModifiedEvent, self.updateGUIFromMRML)
+
+  def setMRMLDefaults(self):
+    super(FastMarchingEffectOptions,self).setMRMLDefaults()
+
+  def updateGUIFromMRML(self,caller,event):
+    super(FastMarchingEffectOptions,self).updateGUIFromMRML(caller,event)
+    self.disconnectWidgets()
+    # TODO: get the march parameter from the node
+    # march = float(self.parameterNode.GetParameter
+
+    self.connectWidgets()
+
+  def onApply(self):
+    print('This is just an example - nothing here yet')
+
+    try:
+      tool = self.tools[0]
+      tool.apply()
+    except IndexError:
+      print('No tools available!')
+      pass
+    
+    self.defaultEffect()
+
+  def onMarcherChanged(self,value):
+    print('Marcher changed!')
+    try:
+      tool = self.tools[0]
+      tool.updateLabel(value)
+    except IndexError:
+      print('No tools available!')
+      pass
+
+  def updateMRMLFromGUI(self):
+    if self.updatingGUI:
+      return
+    disableState = self.parameterNode.GetDisableModifiedEvent()
+    self.parameterNode.SetDisableModifiedEvent(1)
+    super(FastMarchingEffectOptions,self).updateMRMLFromGUI()
+    self.parameterNode.SetDisableModifiedEvent(disableState)
+    if not disableState:
+      self.parameterNode.InvokePendingModifiedEvent()
+
+
+#
+# FastMarchingEffectTool
+#
+
+class FastMarchingEffectTool(LabelEffect.LabelEffectTool):
+  """
+  One instance of this will be created per-view when the effect
+  is selected.  It is responsible for implementing feedback and
+  label map changes in response to user input.
+  This class observes the editor parameter node to configure itself
+  and queries the current view for background and label volume
+  nodes to operate on.
+  """
+
+  def __init__(self, sliceWidget):
+    super(FastMarchingEffectTool,self).__init__(sliceWidget)
+
+    self.fm = None
+
+  def cleanup(self):
+    super(FastMarchingEffectTool,self).cleanup()
+
+  def processEvent(self, caller=None, event=None):
+    """
+    handle events from the render window interactor
+    """
+    if event == "LeftButtonPressEvent":
+      xy = self.interactor.GetEventPosition()
+      sliceLogic = self.sliceWidget.sliceLogic()
+      logic = FastMarchingEffectLogic(sliceLogic)
+      logic.apply(xy)
+      print("Got a %s at %s in %s", (event,str(xy),self.sliceWidget.sliceLogic().GetSliceNode().GetName()))
+      self.abortEvent(event)
+    else:
+      pass
+
+  def apply(self):
+
+    # allocate a new filter each time apply is hit
+    bgImage = self.editUtil.getBackgroundImage()
+    labelImage = self.editUtil.getLabelImage()
+
+    self.fm = slicer.logic.vtkPichonFastMarching()
+    scalarRange = bgImage.GetScalarRange()
+    dim = bgImage.GetWholeExtent()
+    depth = scalarRange[1]-scalarRange[0]
+    self.fm.init(dim[1]+1, dim[3]+1, dim[5]+1, depth, 1, 1, 1)
+    self.fm.SetInput(bgImage)
+    self.fm.SetOutput(labelImage)
+
+    self.fm.setNPointsEvolution(100000)
+    self.fm.setActiveLabel(self.editUtil.getLabel())
+
+    # use all initialized points in the label volume as seeds
+    for i in range(dim[1]+1):
+      for j in range(dim[3]+1):
+        for k in range(dim[5]+1):
+          labelValue = labelImage.GetScalarComponentAsFloat(i,j,k,0)
+          if labelValue:
+            self.fm.addSeedIJK(i,j,k)
+
+    self.fm.Modified()
+    self.fm.Update()
+    
+    print('FastMarching apply called!')
+
+  def updateLabel(self,value):
+    if not self.fm:
+      return
+    self.fm.show(value)
+    self.fm.Modified()
+    self.fm.Update()
+
+    print('FastMarching updated!')
+
+#
+# FastMarchingEffectLogic
+#
+
+class FastMarchingEffectLogic(LabelEffect.LabelEffectLogic):
+  """
+  This class contains helper methods for a given effect
+  type.  It can be instanced as needed by an FastMarchingEffectTool
+  or FastMarchingEffectOptions instance in order to compute intermediate
+  results (say, for user feedback) or to implement the final
+  segmentation editing operation.  This class is split
+  from the FastMarchingEffectTool so that the operations can be used
+  by other code without the need for a view context.
+  """
+
+  def __init__(self,sliceLogic):
+    self.sliceLogic = sliceLogic
+
+  def apply(self,xy):
+    pass
+
+
+#
+# The FastMarchingEffectExtension class definition
+#
+
+class FastMarchingEffectExtension(LabelEffect.LabelEffect):
+  """Organizes the Options, Tool, and Logic classes into a single instance
+  that can be managed by the EditBox
+  """
+
+  def __init__(self):
+    # name is used to define the name of the icon image resource (e.g. FastMarchingEffect.png)
+    self.name = "FastMarchingEffect"
+    # tool tip is displayed on mouse hover
+    self.toolTip = "Paint: circular paint brush for label map editing"
+
+    self.options = FastMarchingEffectOptions
+    self.tool = FastMarchingEffectTool
+    self.logic = FastMarchingEffectLogic
+
+""" Test:
+
+sw = slicer.app.layoutManager().sliceWidget('Red')
+import EditorLib
+pet = EditorLib.FastMarchingEffectTool(sw)
+
+"""
+
+#
+# FastMarchingEffect
+#
+
+class FastMarchingEffect:
+  """
+  This class is the 'hook' for slicer to detect and recognize the extension
+  as a loadable scripted module
+  """
+  def __init__(self, parent):
+    parent.title = "Editor FastMarchingEffect Effect"
+    parent.categories = ["Developer Tools.Editor Extensions"]
+    parent.contributors = ["Andrey Fedorov (BWH)"] # insert your name in the list
+    parent.helpText = """
+    FastMarching segmentation based on work of Eric Pichon.
+    """
+    parent.acknowledgementText = """
+    This editor extension was developed by
+    <Author>, <Institution>
+    based on work by:
+    Steve Pieper, Isomics, Inc.
+    based on work by:
+    Jean-Christophe Fillion-Robin, Kitware Inc.
+    and was partially funded by NIH grant 3P41RR013218.
+    """
+
+    # TODO:
+    # don't show this module - it only appears in the Editor module
+    #parent.hidden = True
+
+    # Add this extension to the editor's list for discovery when the module
+    # is created.  Since this module may be discovered before the Editor itself,
+    # create the list if it doesn't already exist.
+    try:
+      slicer.modules.editorExtensions
+    except AttributeError:
+      slicer.modules.editorExtensions = {}
+    slicer.modules.editorExtensions['FastMarchingEffect'] = FastMarchingEffectExtension
+
+#
+# FastMarchingEffectWidget
+#
+
+class FastMarchingEffectWidget:
+  def __init__(self, parent = None):
+    self.parent = parent
+
+  def setup(self):
+    # don't display anything for this widget - it will be hidden anyway
+    pass
+
+  def enter(self):
+    pass
+
+  def exit(self):
+    pass
+
+
